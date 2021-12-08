c1f94d65b8fe4361bd5656d291aadeca7d91eeb2
manuel <manuel.alexander.hirzel@gmail.com>
2021-12-08 14:06:12 +0100

    eod

---
+++ problem/main.py
@@ -0,0 +1,13 @@
+
+import sys
+import pika 
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("pipr.io",port = 80,virtual_host='rabbit'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+
+print(" [x] Sent 'Hello World!'")
+connection.close()
