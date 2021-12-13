4c180f3e39b07791790c1a949e5e6c35d36f607a
manuel <manuel.alexander.hirzel@gmail.com>
2021-12-13 19:46:35 +0100

    testing submit

--- submit.py
+++ submit.py
@@ -5,14 +5,17 @@ import pika
 import threading
 import sys
 
+
+uuid = sys.argv[1]
+
 connection = pika.BlockingConnection(pika.ConnectionParameters("pipr.io",port = 5672))
 channel = connection.channel()
-channel.queue_declare(queue='statespace', durable=True)
+channel.queue_declare(queue=uuid, durable=True)
 
 states = [{"lr":2.0,"beta":1.2},{"lr":1.2,"beta":0.2}]
 
 for i, state in enumerate(states):
-    channel.basic_publish(exchange='',routing_key='statespace', body=json.dumps(state))
+    channel.basic_publish(exchange='',routing_key=uuid, body=json.dumps(state))
     print(f'published state {i}')
 
 
